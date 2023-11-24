ed59270d09ac1e7ba16aba631680cd15ad54d1a4
Sarvesh Raktawan <sarvesh.raktawan1@gmail.com>
2023-11-24 13:34:04 +0530

    hgj

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,jsonify,request
-
+import pandas as pd
+import pickle
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,24 +12,23 @@ def predict():
     if request.method=='POST':
         make=request.form.get("make")
         model=request.form.get("model")
-        year=request.form.get("year")
-        engine_fuel_type = request.form.get("engine_fuel_type")
-        engine_hp=request.form.get("engine_hp")
-        engine_cylinders=request.form.get("engine_cylinders")
-        transmission_type=request.form.get("transmission_type")
-        driven_wheels=request.form.get("driven_wheels")
-        number_of_doors=request.form.get("number_of_doors")
-        vehicle_size=request.form.get("vehicle_size")
-        vehicle_style=request.form.get("vehicle_style")
-        highway_mpg=request.form.get("highway_mpg")
-        city_mpg=request.form.get("city_mpg")
-        popularity=request.form.get("popularity")
-        print(make,model,year,engine_fuel_type,engine_hp,engine_cylinders,transmission_type,driven_wheels,number_of_doors,vehicle_size,vehicle_style,highway_mpg,city_mpg,popularity)
-        return jsonify({'status':'data fetched successfully'})
+        engine_cylinders=request.form.get("cylinders")
+        driven_wheels=request.form.get("wheels")
+        print(make,model,engine_cylinders,driven_wheels)
+        df=pd.read_json("new.json")
+        Make=df["Make_encode"][df['Make']==make].values[0]
+        Model=df["Model_encode"][df['Model']==model].values[0]
+        Driven_wheels=df["Driven_Wheels_encode"][df['Driven_Wheels']==driven_wheels].values[0]
+        with open('model.pkl', 'rb') as mod:
+            mlmodel = pickle.load(mod)
+
+        predit = mlmodel.predict([[Model,Make,float(engine_cylinders),Driven_wheels]])
+
+        return render_template('predicted.html',predicted_value=predit[0])
+        
     else:
         return render_template("predict.html")
-    return render_template('predict.html')
-
+   
 
  
 
